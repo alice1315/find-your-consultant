a6391aa92b3773f89669872474972de9147a771d
Alice Huang <viento.a13@gmail.com>
2022-05-13 16:43:25 +0800

    Finished field.html.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from app.index import index as index_bp
+from app.main import main as main_bp
 from app.auth import auth as auth_bp
 
 def create_app():
@@ -8,7 +8,7 @@ def create_app():
     app.config["JSON_AS_ASCII"] = False
     app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-    app.register_blueprint(index_bp)
+    app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
 
     return app
