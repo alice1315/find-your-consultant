15e692da65b38a3dd1904893cd1e68067d8f1c6f
Alice Huang <viento.a13@gmail.com>
2022-06-05 02:51:38 +0800

    Test socket.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+import eventlet
+eventlet.monkey_patch()
+
 from .models.response import Response
 from .models.database import Database
 from .models.s3 import S3
@@ -33,6 +36,6 @@ def create_app():
     app.register_blueprint(case_bp)
     app.register_blueprint(api_bp, url_prefix = "/api")
 
-    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.init_app(app, async_mode = "eventlet", cors_allowed_origins="*")
 
     return app
